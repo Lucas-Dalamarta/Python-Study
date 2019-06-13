44a49a6238e552f5b57f82df51bd9d9aaa65b735
Lucas Felipe Dalamarta <lfdalamarta@homtail.com>
2019-06-12 22:53:19 -0300

    Better Calc

--- Randoms/Calculator.py
+++ Randoms/Calculator.py
@@ -21,20 +21,13 @@ def _mul(a,b):
     return  print("\t "+str(a)+" X "+str(b)+" = "+str(a*b))
 
 
-def _Line(o):
-    """Prints a line of chars , given chosen char """
+def _Line(v_char):
+    """Prints a line of chars , given chosen char  """
     c = ""
     for i in range(40):
-        if o == "=" :
-            c = c + "="
-        elif o == "-" :
-            c = c + "-"
-        elif o == "*" :
-            c = c + "*"
-        else :
-            c = c + " "
-
+        c = c + v_char
     return print(c)
+
 
 def menu():
     _Line("=")
@@ -43,31 +36,42 @@ def menu():
     print("\tEnter [3] to   Div ")
     print("\tEnter [4] to   Mul ")
     _Line("=")
+
 #-------------------------------------
-while True:
-    choice = 0
+def main():
+    """Old habits ..."""
+    while True:
+        choice = 0
 
-    a = int(input("Enter a number :\t"))
-    b = int(input("Enter another number :\t"))
+        a = int(input("Enter a number :\t"))
+        b = int(input("Enter another number :\t"))
 
-    menu()
+        menu()
 
-    choice = int(input("Option :\t"))
+        choice = int(input("Option :\t"))
+        
+        _Line("*")
 
-    if choice   == 1:
-        _sum(a,b)
-    elif choice == 2:
-        _sub(a,b)
-    elif choice == 3:
-        _div(a,b)
-    elif choice == 4:
-        _mul(a,b)
-    
-    _Line("*")
-    choice = int(input("Do you want to go again ? [0/1] \t"))
-    _Line("*")
-    
-    if choice == 0:
-        break
-    else:
-        pass
+        if choice   == 1:
+            _sum(a,b)
+        elif choice == 2:
+            _sub(a,b)
+        elif choice == 3:
+            _div(a,b)
+        elif choice == 4:
+            _mul(a,b)
+        
+        _Line("*")
+        choice = int(input("Do you want to go again ? [0/1] \t"))
+        _Line("*")
+        
+        if choice != 1:
+            break
+        else:
+            pass
+#-------------------------------
+_Line(":")
+main()
+#-------------------------------
+print("\tApplication Ended !")
+_Line(":")
