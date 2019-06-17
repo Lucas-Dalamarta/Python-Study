a899b907c6e797f12259b01ded2a716d5796d051
Lucas Felipe Dalamarta <lfdalamarta@homtail.com>
2019-06-16 22:58:24 -0300

    Adjustments

--- WeekDay/WeekDay.py
+++ WeekDay/WeekDay.py
@@ -4,14 +4,15 @@
 #                                                       #
 #»«»«»«»«»«»«»«»«»«»«»«»«»«»«»«»«»«»«»«»«»«»«»«»«»«»«»«»#
 
-def _Line(v_char):
+def _Line(v_char,v_lengh):
     """Prints a line of chars , given chosen char  """
     c = ""
-    for i in range(40):
+    for i in range(v_lengh):
         c = c + v_char
-    return print(c)
+    return print("\n"+c+"\n")
  
-def _try_again():
+
+def _tryAgain():
     """Gently asks the user to Try Again"""
     return print("\tIncorrect Value , please try again")
 
@@ -25,7 +26,7 @@ def getMonth():
             if v_Month == 0 or v_Month > 12 :
                 raise   ValueError
         except ValueError :
-            _try_again()
+            _tryAgain()
             continue
         break
     return v_Month
@@ -48,7 +49,7 @@ def getDay(a_Month):
                 if v_Day == 0 or v_Day > 29 :
                     raise   ValueError
         except ValueError:
-            _try_again()
+            _tryAgain()
             continue
         break
     return v_Day
@@ -64,7 +65,7 @@ def getYear():
             if v_year == 0 or v_year > 9999 :
                 raise ValueError
         except ValueError:
-            _try_again()
+            _tryAgain()
             continue
         break
     return v_year
@@ -76,7 +77,7 @@ def getLeap(a_year):
 
     if (a_year % 4 == 0 and a_year % 100 > 0) or a_year % 400 == 0 :  
         v_leap = True
-    else    :                                                   
+    else :
         v_leap = False
     return v_leap
 
@@ -99,37 +100,37 @@ def getValue(p_Month , p_Day , p_Year_digits ,p_Century , p_leap):
     Value   =   p_Year_digits + (p_Year_digits // 4)
 
     if p_Century == 18  : 
-        Value = Value + 2
-    elif    p_Century == 20  : 
+        Value += 2
+    elif p_Century == 20  : 
         Value += 6
 
     if Month == 1 :
         if p_leap == False :
-            Value = Value + 1
+            Value += 1
     elif Month == 2 :
         if p_leap == True :
-            Value = Value + 3
+            Value += 3
         else    :
-            Value = Value + 4
+            Value += 4
     elif Month == 3 or Month == 11  :
-        Value = Value + 4
+        Value += 4
     elif Month == 4 or Month == 7   :
         Value += 0
     elif Month == 5 :
-        Value = Value + 2
+        Value += 2
     elif Month == 6 :
-        Value = Value + 5
+        Value += 5
     elif Month == 8 :
-        Value = Value + 3
+        Value += 3
     elif Month == 10 :
-        Value = Value + 1
+        Value += 1
     elif Month == 9 or Month == 12 :
-        Value = Value + 6
+        Value += 6
 
     return abs((Value+Day) % 7)
 
 
-def return_Day(v_value):
+def returnDay(v_value):
     """Returns which day it was , given the value calculated past"""
     weekday = ""
 
@@ -151,11 +152,13 @@ def return_Day(v_value):
 
 
 #»«»«»«»«»«»«»«»«»«»«»«»«»«»«»«»«»«»«»«»«»«»«»«»«»«»«»«»#
-_Line("=")
+_Line("=",60)
+print("\t\tDate format MM/DD/YYYY")
+_Line("=",60)
 Month           =   getMonth()
 Day             =   getDay(Month)
 Year            =   getYear()
-_Line("*")
-print("On ",Month,"/",Day,"/",Year," it was a : ",return_Day(getValue(Month,Day,getYearDigits(Year),getCentury(Year),getLeap(Year))))
-_Line("=")
+_Line("#",60)
+print("\tOn ",Month,"/",Day,"/",Year," it was a ",returnDay(getValue(Month,Day,getYearDigits(Year),getCentury(Year),getLeap(Year))),"!")
+_Line("#",60)
 #»«»«»«»«»«»«»«»«»«»«»«»«»«»«»«»«»«»«»«»«»«»«»«»«»«»«»«»#
